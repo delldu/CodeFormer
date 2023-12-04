b3022e99603ae783d6703efa3eeef44d7b46c24c
dell du <18588220928@163>
2023-12-04 23:07:42 +0800

    Update

--- project/image_face/__init__.py
+++ project/image_face/__init__.py
@@ -47,7 +47,7 @@ def get_face_model():
     return model, device
 
 
-def face_predict(input_files, output_dir, output_only_hasface=False):
+def face_predict(input_files, output_dir):
     # Create directory to store result
     todos.data.mkdir(output_dir)
 
@@ -68,26 +68,21 @@ def face_predict(input_files, output_dir, output_only_hasface=False):
         with torch.no_grad():
             predict_tensor, detected_tensor = model(input_tensor.to(device))
 
-        if output_only_hasface and detected_tensor.shape[0] < 2:
-            continue
 
-        output_file = f"{output_dir}/{os.path.basename(filename)}"
+        # output_file = f"{output_dir}/{os.path.basename(filename)}"
+        # # B, C, H, W = input_tensor.size()
+        # # output_tensor = todos.data.resize_tensor(input_tensor, H, W)
+        # todos.data.save_tensor([output_tensor, predict_tensor], output_file)
 
-        B, C, H, W = input_tensor.size()
-        output_tensor = todos.data.resize_tensor(input_tensor, H, W)
-
-        todos.data.save_tensor([output_tensor, predict_tensor], output_file)
-
-        if not output_only_hasface: # output detected face for debug
-            # save detected faces
-            output_file = f"{output_dir}/detect_{os.path.basename(filename)}"
-            if detected_tensor.size(0) < 2:
-                todos.data.save_tensor([detected_tensor], output_file)
-            else:
-                n_row = math.ceil(math.sqrt(detected_tensor.size(0)))
-                if n_row % 2 != 0:
-                    n_row = n_row + 1
-                grid_image = todos.data.grid_image(list(torch.split(detected_tensor, 1, dim=0)), nrow=n_row)
-                grid_image.save(output_file)
+        # save detected faces
+        output_file = f"{output_dir}/d_{os.path.basename(filename)}"
+        if detected_tensor.size(0) < 2:
+            pass # todos.data.save_tensor([detected_tensor], output_file)
+        else:
+            n_row = math.ceil(math.sqrt(detected_tensor.size(0)))
+            if n_row % 2 != 0:
+                n_row = n_row + 1
+            grid_image = todos.data.grid_image(list(torch.split(detected_tensor, 1, dim=0)), nrow=n_row)
+            grid_image.save(output_file)
 
     todos.model.reset_device()
